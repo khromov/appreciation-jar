2165332a1ba18e19c44f6009a83e09635fec950d
Stanislav Khromov <stanislav+github@khromov.se>
2022-07-11 03:32:24 +0200

    Add attributions, replace error image

--- src/templates/error.php
+++ src/templates/error.php
@@ -4,7 +4,7 @@ echo $this->fetch('./partial/header.php');
 ?>
 <div style="width: 650px;" class="max-w-full mb-auto mx-auto px-4 shrink-0">
     <div class="mx-auto mt-4 border-2 rounded-lg bg-white pb-4">
-        <img style="max-width: 200px" class="mx-auto mt-8" src="<?php echo $baseFolder; ?>/images/flame-1698.png" alt="three hearts" />
+        <img style="max-width: 200px" class="mx-auto mt-8" src="<?php echo $baseFolder; ?>/images/bermuda-465.png" alt="three hearts" />
         <h1 class="text-4xl font-bold text-center mt-4 px-6">Error</h1>
         <h1 class="text-2xl font-bold text-center mt-4 px-6">
             <?php echo $errorMessage ? $escaper->escapeHtml($errorMessage) : 'Unknown error'; ?>
--- src/templates/partial/footer.php
+++ src/templates/partial/footer.php
@@ -1,5 +1,9 @@
     <div class="mt-2 h-10 text-white text-center color-white pb-2 w-full">
-        Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
+        Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from 
+        <a href="https://icons8.com/illustrations/style--arabica">Arabica</a>, 
+        <a href="https://icons8.com/illustrations/">Ouch!</a>, 
+        <a href="https://icons8.com/illustrations/style--bermuda-1">Bermuda</a> & 
+        <a href="https://icons8.com/illustrations/style--woolly">Woolly</a>
     </div>
 </div>
 </body>
