fe75488159e7fca6f2674eb9ecf7006557e32777
Stanislav Khromov <stanislav+github@khromov.se>
2022-07-11 03:08:31 +0200

    Vertical center latest card, increase text size

--- src/templates/latest.php
+++ src/templates/latest.php
@@ -2,11 +2,15 @@
 $escaper = new Laminas\Escaper\Escaper('utf-8');
 echo $this->fetch('./partial/header.php'); 
 ?>
-<div style="width: 650px;" class="max-w-full mb-auto mx-auto px-4 shrink-0">
-    <?php echo $this->fetch('./partial/appreciation-card.php', ['appreciation' => $appreciation]); ?>
-    <div class="text-center pt-2">
-        <a class="text-white" href="<?php echo $basePath ?>/archive">🗓️ View all published</a> <span class="text-white">➖</span>
-        <a class="text-white" href="<?php echo $basePath ? $basePath : '/' ?>">🌱 Create new</a>
+<div style="width: 650px;" class="max-w-full mx-auto px-4 grow">
+    <div class="flex flex-col justify-center h-full"> <!-- style="width: 650px;"  -->
+        <div>
+            <?php echo $this->fetch('./partial/appreciation-card.php', ['appreciation' => $appreciation]); ?>
+            <div class="text-center pt-2">
+                <a class="text-white" href="<?php echo $basePath ?>/archive">🗓️ View all published</a>
+                <a class="text-white" href="<?php echo $basePath ? $basePath : '/' ?>">🌱 Create new</a>
+            </div>
+        </div>
     </div>
 </div>
 <script>
--- src/templates/partial/appreciation-card.php
+++ src/templates/partial/appreciation-card.php
@@ -4,10 +4,10 @@ $escaper = new Laminas\Escaper\Escaper('utf-8');
 <div class="mx-auto mt-4 border-2 rounded-lg bg-white pb-4">
     <img style="max-width: 200px" class="mx-auto mt-8" src="<?php echo $baseFolder; ?>/images/arabica-452.png" alt="three hearts" />
 
-    <h1 class="text-2xl font-bold text-center mt-4 px-6">
+    <h1 class="text-4xl font-bold text-center mt-4 px-6">
         <?php echo $escaper->escapeHtml($appreciation['text']); ?>
     </h1>
-    <p class="mt-4 text-center px-6">
+    <p class="mt-4 text-center text-lg px-6">
         <?php echo $escaper->escapeHtml($appreciation['author']); ?>
     </p>
     <p class="mt-4 mb-6 text-center px-6 italic">
